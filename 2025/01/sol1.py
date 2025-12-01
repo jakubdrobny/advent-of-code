c9b7db72aee8a53324be8945f9fcf0e7b8e062fd
jakubdrobny <jkubeing@gmail.com>
2025-12-01 10:02:01 +0100

    fix day 1 part 1

--- 2025/01/sol1.py
+++ 2025/01/sol1.py
@@ -7,8 +7,9 @@ ans = 0
 for line in sys.stdin:
     x = int(line[1:])
     if line[0] == "L":
-        x = N - x
-    pos = (pos + x) % N
+        pos = ((pos - x) % N + N) % N
+    else:
+        pos = (pos + x) % N
     if pos == 0:
         ans += 1
 
